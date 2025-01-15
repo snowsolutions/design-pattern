0d4458d35ae41f26159ba23e3a4a61c2467a885a
Frank Nguyen <nguyen.hongphuc@creditorwatch.com.au>
2025-01-15 15:25:22 +0700

    Hexagonal init -- first commit

--- Hexagonal/FeatureFlag/bootstrap.php
+++ Hexagonal/FeatureFlag/bootstrap.php
@@ -7,7 +7,7 @@ use App\Services\FeatureFlagService;
 if (!function_exists('get_target_framework')) {
     function get_target_framework()
     {
-        $ldFeatureFlagService = new FeatureFlagService('sdk-9930dbaf-2218-4616-b865-a5349423ecc5');
+        $ldFeatureFlagService = new FeatureFlagService(getenv('LD_CLIENT_KEY'));
         return $ldFeatureFlagService->isFlagEnable(FeatureFlag::FEATURE_FLAG_USE_FRAMEWORK);
     }
 }
